57baf1a9acc659797188ffc096cabe298764cf43
Rex <r.fleur@student.tudelft.nl>
2025-05-02 18:36:41 +0200

    improved readability

--- src/brickwork_transpiler/graph_builder.py
+++ src/brickwork_transpiler/graph_builder.py
@@ -1,12 +1,15 @@
 import networkx as nx
 
-
+# Generates a single brick row graph
 def create_tuple_node_graph(rows, cols):
+
     G = nx.Graph()
+
     for i in range(rows):
         for j in range(cols):
             G.add_node((i, j))
     for i in range(rows):
         for j in range(cols-1):
             G.add_edge((i, j), (i, j+1))
+
     return G
