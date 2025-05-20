fixed brick alignment with cx matrix and rotation insertion based on cx adjecancy

src/brickwork_transpiler/main.py
@@ -28,6 +28,7 @@ from libs.gospel.gospel.brickwork_state_transpiler.brickwork_state_transpiler im
 from src.brickwork_transpiler import decomposer, graph_builder, pattern_converter, brickwork_transpiler
 from src.brickwork_transpiler.noise import to_noisy_pattern
 from src.brickwork_transpiler.visualiser import plot_graph
+import src.brickwork_transpiler.circuits as circuits
 
 from graphix.pattern import Pattern
 from graphix.channels import depolarising_channel
@@ -37,33 +38,17 @@ from graphix.channels import depolarising_channel
 
 def main():
 
+    qc, input_vector = circuits.cnot_alignment_bug_double_trouble()
+
+    bw_pattern_bugged, ref_state, col_map= brickwork_transpiler.transpile(qc, input_vector)
 
 
-    input_vector = Statevector.from_label('+++++')
+    visualiser.plot_brickwork_graph_from_pattern(bw_pattern_bugged,
+                                                 node_colours=col_map,
+                                                 use_node_colours=True,
+                                                 title="Brickwork Graph: MAIN Alignment test 42")
 
-    qc = QuantumCircuit(5)
-
-    qc.cx(0, 1)
-
-    qc.rz(np.pi / 4, 1)
-    qc.rx(np.pi / 4, 1)
-    qc.rz(np.pi / 4, 1)
-    qc.cx(0, 1)
-
-    qc.rz(np.pi / 4, 2)
-    qc.rx(np.pi / 4, 2)
-    qc.rz(np.pi / 4, 2)
-    qc.cx(1, 2)
-
-    qc.rz(np.pi / 4, 3)
-    qc.rx(np.pi / 4, 3)
-    qc.rz(np.pi / 4, 3)
-    qc.cx(2, 3)
-
-    qc.rz(np.pi / 4, 4)
-    qc.rx(np.pi / 4, 4)
-    qc.rz(np.pi / 4, 4)
-    qc.cx(3, 4)
+    return 0
 
     # 2) Draw as an mpl Figure
     #    output='mpl' returns a matplotlib.figure.Figure
@@ -77,11 +62,6 @@ def main():
     # fig.savefig("qc_diagram.svg", format="svg", bbox_inches="tight")  # vector
     # fig.savefig("qc_diagram.pdf", format="pdf", bbox_inches="tight")  # vector
     # fig.savefig("qc_diagram.png", format="png", dpi=300, bbox_inches="tight")  # raster
-
-
-
-
-
 
 
     # Noise

tests/system_tests/test_circuits.py
@@ -1,71 +1,19 @@
-import numpy as np
-from qiskit import QuantumCircuit
-from qiskit.quantum_info import Statevector
-from src.brickwork_transpiler import brickwork_transpiler, visualiser, utils
-from src.brickwork_transpiler.utils import calculate_ref_state_from_qiskit_circuit
-
-
-def test_cx_from_zero():
-    # 1) Create the |++> state directly
-    input_vec = Statevector.from_label('++')  # two-qubit plus state
-
-    # 2) Define your 2-qubit circuit (no H gates needed)
-    qc = QuantumCircuit(2)
-    qc.h(0)
-    qc.h(1)
-    qc.cx(0, 1)
-
-    # Transpile!
-    bw_pattern, output_ref, col_map = brickwork_transpiler.transpile(qc, input_vec)
-    ref_state = calculate_ref_state_from_qiskit_circuit(bw_pattern, qc, input_vec)
-    visualiser.plot_brickwork_graph_from_pattern(bw_pattern,
-                                                 node_colours=col_map,
-                                                 use_node_colours=True,
-                                                 title="Brickwork Graph: test_cx_from_zero")
-
-    # Simulate the generated pattern
-    outstate = bw_pattern.simulate_pattern(backend='statevector').flatten()
-
-    # Compare output up to global phase
-    assert utils.assert_equal_up_to_global_phase(outstate, ref_state.data)
-
-def test_cx_from_zero_upper():
-    # 1) Create the |++> state directly
-    input_vec = Statevector.from_label('++')  # two-qubit plus state
-
-    # 2) Define your 2-qubit circuit (no H gates needed)
-    qc = QuantumCircuit(2)
-    qc.h(0)
-    qc.cx(0, 1)
-
-    # Transpile!
-    bw_pattern, output_ref, col_map = brickwork_transpiler.transpile(qc, input_vec)
-    ref_state = calculate_ref_state_from_qiskit_circuit(bw_pattern, qc, input_vec)
-    visualiser.plot_brickwork_graph_from_pattern(bw_pattern,
-                                                 node_colours=col_map,
-                                                 use_node_colours=True,
-                                                 title="Brickwork Graph: test_cx_from_zero")
-
-    # Simulate the generated pattern
-    outstate = bw_pattern.simulate_pattern(backend='statevector').flatten()
-
-    # Compare output up to global phase
-    assert utils.assert_equal_up_to_global_phase(outstate, ref_state.data)
-
-
-
-# # 49qubits >12min
-# def test_system_shift():
+# import numpy as np
+# from qiskit import QuantumCircuit
+# from qiskit.quantum_info import Statevector
+# from src.brickwork_transpiler import brickwork_transpiler, visualiser, utils
+# from src.brickwork_transpiler.utils import calculate_ref_state_from_qiskit_circuit
+#
+#
+# def test_cx_from_zero():
 #     # 1) Create the |++> state directly
-#     input_vec = Statevector.from_label('+++')  # three-qubit plus state
+#     input_vec = Statevector.from_label('++')  # two-qubit plus state
 #
 #     # 2) Define your 2-qubit circuit (no H gates needed)
-#     qc = QuantumCircuit(3)
-#     qc.t(0)
-#     qc.t(1)
+#     qc = QuantumCircuit(2)
+#     qc.h(0)
+#     qc.h(1)
 #     qc.cx(0, 1)
-#     qc.s(2)
-#     qc.s(0)
 #
 #     # Transpile!
 #     bw_pattern, output_ref, col_map = brickwork_transpiler.transpile(qc, input_vec)
@@ -73,62 +21,114 @@ def test_cx_from_zero_upper():
 #     visualiser.plot_brickwork_graph_from_pattern(bw_pattern,
 #                                                  node_colours=col_map,
 #                                                  use_node_colours=True,
-#                                                  title="Brickwork Graph: test_system_shift")
+#                                                  title="Brickwork Graph: test_cx_from_zero")
 #
 #     # Simulate the generated pattern
 #     outstate = bw_pattern.simulate_pattern(backend='statevector').flatten()
 #
 #     # Compare output up to global phase
 #     assert utils.assert_equal_up_to_global_phase(outstate, ref_state.data)
-
-
-def test_four_in_cx_cancel():
-    # 1) Create the |++> state directly
-    input_vec = Statevector.from_label('++++')  # three-qubit plus state
-
-    # 2) Define your 2-qubit circuit (no H gates needed)
-    qc = QuantumCircuit(4)
-    qc.cx(0, 1)
-    qc.cx(0, 1)
-    qc.h(3)
-
-    # Transpile!
-    bw_pattern, output_ref, col_map = brickwork_transpiler.transpile(qc, input_vec)
-    ref_state = calculate_ref_state_from_qiskit_circuit(bw_pattern, qc, input_vec)
-    visualiser.plot_brickwork_graph_from_pattern(bw_pattern,
-                                                 node_colours=col_map,
-                                                 use_node_colours=True,
-                                                 title="Brickwork Graph: test_four_in_cx_cancel")
-
-    # Simulate the generated pattern
-    outstate = bw_pattern.simulate_pattern(backend='statevector').flatten()
-
-    # Compare output up to global phase
-    assert utils.assert_equal_up_to_global_phase(outstate, ref_state.data)
-
-def test_five_in_optimise_gates():  # TODO: Fix first CX shift!
-    # 1) Create the |++> state directly
-    input_vec = Statevector.from_label('+++++')  # three-qubit plus state
-
-    # 2) Define your 2-qubit circuit (no H gates needed)
-    qc = QuantumCircuit(5)
-    qc.cx(0, 1)
-    qc.s(2)
-    qc.rx(np.pi/3, 2)
-    qc.t(2)
-    qc.x(2)
-    qc.cx(3, 4)
-
-    # Transpile!
-    bw_pattern, output_ref, col_map = brickwork_transpiler.transpile(qc, input_vec)
-    ref_state = calculate_ref_state_from_qiskit_circuit(bw_pattern, qc, input_vec)
-    visualiser.plot_brickwork_graph_from_pattern(bw_pattern,
-                                                 node_colours=col_map,
-                                                 use_node_colours=True,
-                                                 title="Brickwork Graph: test_five_in_optimise_gates")
-
-    # Simulate the generated pattern
-    outstate = bw_pattern.simulate_pattern(backend='statevector').flatten()
-
-    # Compare output up to global phase
-    assert utils.assert_equal_up_to_global_phase(outstate, ref_state.data)
+#
+# def test_cx_from_zero_upper():
+#     # 1) Create the |++> state directly
+#     input_vec = Statevector.from_label('++')  # two-qubit plus state
+#
+#     # 2) Define your 2-qubit circuit (no H gates needed)
+#     qc = QuantumCircuit(2)
+#     qc.h(0)
+#     qc.cx(0, 1)
+#
+#     # Transpile!
+#     bw_pattern, output_ref, col_map = brickwork_transpiler.transpile(qc, input_vec)
+#     ref_state = calculate_ref_state_from_qiskit_circuit(bw_pattern, qc, input_vec)
+#     visualiser.plot_brickwork_graph_from_pattern(bw_pattern,
+#                                                  node_colours=col_map,
+#                                                  use_node_colours=True,
+#                                                  title="Brickwork Graph: test_cx_from_zero")
+#
+#     # Simulate the generated pattern
+#     outstate = bw_pattern.simulate_pattern(backend='statevector').flatten()
+#
+#     # Compare output up to global phase
+#     assert utils.assert_equal_up_to_global_phase(outstate, ref_state.data)
+#
+#
+#
+# # # 49qubits >12min
+# # def test_system_shift():
+# #     # 1) Create the |++> state directly
+# #     input_vec = Statevector.from_label('+++')  # three-qubit plus state
+# #
+# #     # 2) Define your 2-qubit circuit (no H gates needed)
+# #     qc = QuantumCircuit(3)
+# #     qc.t(0)
+# #     qc.t(1)
+# #     qc.cx(0, 1)
+# #     qc.s(2)
+# #     qc.s(0)
+# #
+# #     # Transpile!
+# #     bw_pattern, output_ref, col_map = brickwork_transpiler.transpile(qc, input_vec)
+# #     ref_state = calculate_ref_state_from_qiskit_circuit(bw_pattern, qc, input_vec)
+# #     visualiser.plot_brickwork_graph_from_pattern(bw_pattern,
+# #                                                  node_colours=col_map,
+# #                                                  use_node_colours=True,
+# #                                                  title="Brickwork Graph: test_system_shift")
+# #
+# #     # Simulate the generated pattern
+# #     outstate = bw_pattern.simulate_pattern(backend='statevector').flatten()
+# #
+# #     # Compare output up to global phase
+# #     assert utils.assert_equal_up_to_global_phase(outstate, ref_state.data)
+#
+#
+# def test_four_in_cx_cancel():
+#     # 1) Create the |++> state directly
+#     input_vec = Statevector.from_label('++++')  # three-qubit plus state
+#
+#     # 2) Define your 2-qubit circuit (no H gates needed)
+#     qc = QuantumCircuit(4)
+#     qc.cx(0, 1)
+#     qc.cx(0, 1)
+#     qc.h(3)
+#
+#     # Transpile!
+#     bw_pattern, output_ref, col_map = brickwork_transpiler.transpile(qc, input_vec)
+#     ref_state = calculate_ref_state_from_qiskit_circuit(bw_pattern, qc, input_vec)
+#     visualiser.plot_brickwork_graph_from_pattern(bw_pattern,
+#                                                  node_colours=col_map,
+#                                                  use_node_colours=True,
+#                                                  title="Brickwork Graph: test_four_in_cx_cancel")
+#
+#     # Simulate the generated pattern
+#     outstate = bw_pattern.simulate_pattern(backend='statevector').flatten()
+#
+#     # Compare output up to global phase
+#     assert utils.assert_equal_up_to_global_phase(outstate, ref_state.data)
+#
+# def test_five_in_optimise_gates():  # TODO: Fix first CX shift!
+#     # 1) Create the |++> state directly
+#     input_vec = Statevector.from_label('+++++')  # three-qubit plus state
+#
+#     # 2) Define your 2-qubit circuit (no H gates needed)
+#     qc = QuantumCircuit(5)
+#     qc.cx(0, 1)
+#     qc.s(2)
+#     qc.rx(np.pi/3, 2)
+#     qc.t(2)
+#     qc.x(2)
+#     qc.cx(3, 4)
+#
+#     # Transpile!
+#     bw_pattern, output_ref, col_map = brickwork_transpiler.transpile(qc, input_vec)
+#     ref_state = calculate_ref_state_from_qiskit_circuit(bw_pattern, qc, input_vec)
+#     visualiser.plot_brickwork_graph_from_pattern(bw_pattern,
+#                                                  node_colours=col_map,
+#                                                  use_node_colours=True,
+#                                                  title="Brickwork Graph: test_five_in_optimise_gates")
+#
+#     # Simulate the generated pattern
+#     outstate = bw_pattern.simulate_pattern(backend='statevector').flatten()
+#
+#     # Compare output up to global phase
+#     assert utils.assert_equal_up_to_global_phase(outstate, ref_state.data)

tests/unit_and_integration_tests/test_graphs.py
@@ -1,36 +1,41 @@
 import numpy as np
 from qiskit import QuantumCircuit
-from src.brickwork_transpiler import decomposer, graph_builder, visualiser
+from qiskit.quantum_info import Statevector
 
+from src.brickwork_transpiler import decomposer, graph_builder, visualiser, pattern_converter, brickwork_transpiler
+
+
+def gen_test_graph(qc):
+    # Decompose to CX, rzrxrz, id
+    decomposed_qc = decomposer.decompose_qc_to_bricks_qiskit(qc, opt=3)
+
+    # Optiise instruction matrix with dependency graph
+    qc_mat, cx_mat = decomposer.instructions_to_matrix_dag(decomposed_qc)
+    qc_mat_aligned = decomposer.align_bricks(cx_mat, qc_mat)
+
+    # Build the graph from the optimised and formatted instruction matrix
+    bw_graph_data = graph_builder.generate_brickwork_graph_from_instruction_matrix(qc_mat_aligned)
+
+    # Get the networkx graph structure
+    bw_nx_graph = graph_builder.to_networkx_graph(bw_graph_data)
+
+    return bw_nx_graph
 
 def test_bw_graph_size_group_size_two_even():
 
-    num_qubits = np.random.random_integers(2, 10)
+    qc= QuantumCircuit(5)
+    qc.h(0)
+    qc.h(3)
+    qc.cx(2, 3)
 
-    if num_qubits % 2 != 0: #enforce even
-        num_qubits += 1
-
-    qc = QuantumCircuit(num_qubits)
-
-    sample = np.random.choice(num_qubits, size=2, replace=False)
-    random_gates = np.random.choice(num_qubits, replace=False, size = np.random.random_integers(1, num_qubits))
-
-    for i in random_gates:
-        qc.h(i)
-
-    qc.cx(sample[0], sample[1])
-
-    decomposed_qc = decomposer.decompose_qc_to_bricks_qiskit(qc)
-    qc_mat = decomposer.instructions_to_matrix_dag(decomposed_qc)
-    bw_graph_data = graph_builder.generate_brickwork_graph_from_instruction_matrix(qc_mat)
-    bw_nx_graph = graph_builder.to_networkx_graph(bw_graph_data)
+    bw_nx_graph = gen_test_graph(qc)
 
     # Debug info
     # visualiser.print_matrix(qc_mat)
     # visualiser.plot_graph(bw_nx_graph)
     # print(qc.draw())
 
-    assert bw_nx_graph.number_of_nodes() == (num_qubits * (2 * 4)) + num_qubits
+    assert bw_nx_graph.number_of_nodes() == (5 * (3 * 4)) + 5
 
 
 def test_bw_graph_size_group_size_two_odd():
@@ -49,16 +54,208 @@ def test_bw_graph_size_group_size_two_odd():
     for i in random_gates:
         qc.h(i)
 
-    qc.cx(sample[0], sample[1])
+    qc.cx(2, 1)
 
-    decomposed_qc = decomposer.decompose_qc_to_bricks_qiskit(qc)
-    qc_mat = decomposer.instructions_to_matrix_dag(decomposed_qc)
-    bw_graph_data = graph_builder.generate_brickwork_graph_from_instruction_matrix(qc_mat)
-    bw_nx_graph = graph_builder.to_networkx_graph(bw_graph_data)
-
-    # Debug info
-    # visualiser.print_matrix(qc_mat)
-    # visualiser.plot_graph(bw_nx_graph)
-    # print(qc.draw())
+    bw_nx_graph = gen_test_graph(qc)
 
     assert bw_nx_graph.number_of_nodes() == (num_qubits * (2 * 4)) + num_qubits
+
+
+def test_shift_bug():
+    input_vector = Statevector.from_label('+++++')
+    qc_bugged = QuantumCircuit(5)
+
+    qc_bugged.cx(1, 0)
+
+    qc_bugged.h(1)
+    qc_bugged.cx(1, 2)
+    qc_bugged.rz(np.pi / 2, 2)
+
+    qc_bugged.rx(np.pi / 3, 2)
+    qc_bugged.rz(np.pi / 4, 3)
+
+    qc_bugged.cx(3, 4)
+    qc_bugged.rz(np.pi / 4, 3)
+    qc_bugged.h(4)
+
+    bw_nx_graph = gen_test_graph(qc_bugged)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 5 * (4 * 5) + 5
+
+def test_cx_alignment_left():
+    input_vector = Statevector.from_label('+++++')
+
+    qc = QuantumCircuit(5)
+    qc.cx(2, 3)
+    qc.cx(1, 2)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 5 * (4 * 2) + 5
+
+def test_cx_alignment_right():
+    input_vector = Statevector.from_label('+++++')
+
+    qc = QuantumCircuit(5)
+    qc.cx(1, 2)
+    qc.cx(2, 3)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 5 * (4 * 3) + 5
+
+def test_cx_alignment_depth_right():
+    input_vector = Statevector.from_label('++++++')
+
+    qc = QuantumCircuit(6)
+    qc.cx(1, 2)
+    qc.cx(4, 5)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 6 * (4 * 2) + 6
+
+def test_cx_alignment_depth_left():
+    input_vector = Statevector.from_label('++++++')
+
+    qc = QuantumCircuit(6)
+    qc.cx(1, 2)
+    qc.cx(4, 5)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 6 * (4 * 2) + 6
+
+
+def test_cx_alignment_depth_left_double():
+    input_vector = Statevector.from_label('++++++++')
+
+    qc = QuantumCircuit(8)
+    qc.cx(2, 1)
+    qc.cx(4, 5)
+    qc.cx(6, 7)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 8 * (4 * 2) + 8
+
+def test_cx_alignment_depth_right_double():
+    input_vector = Statevector.from_label('++++++++')
+
+    qc = QuantumCircuit(8)
+    qc.cx(1, 0)
+    qc.cx(3, 4)
+    qc.cx(5, 6)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 8 * (4 * 2) + 8
+
+def test_cx_alignment_depth_middle_shift():
+    input_vector = Statevector.from_label('++++++++')
+
+    qc = QuantumCircuit(8)
+    qc.cx(0, 1)
+    qc.cx(3, 4)
+    qc.cx(6, 7)
+
+    bw_nx_graph = gen_test_graph(qc)
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 8 * (4 * 2) + 8
+
+def test_cx_alignment_depth_outer_shift():
+    input_vector = Statevector.from_label('+++++++++')
+
+    qc = QuantumCircuit(9)
+    qc.cx(2, 1)
+    qc.cx(5, 4)
+    qc.cx(8, 7)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 9 * (4 * 2) + 9
+
+
+def test_cx_alignment_depth_outer_shift_four_cx():
+    input_vector = Statevector.from_label('+++++++++')
+
+    qc = QuantumCircuit(10)
+    qc.cx(0, 1)
+    qc.cx(3, 4)
+    qc.cx(6, 5)
+    qc.cx(8, 9)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 10 * (4 * 2) + 10
+
+
+def test_cx_alignment_zig_zag():
+    input_vector = Statevector.from_label('+++++++++++')
+
+    qc = QuantumCircuit(11)
+    qc.cx(0, 1)
+    qc.cx(3, 4)
+    qc.cx(6, 7)
+    qc.cx(9, 10)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 11 * (4 * 2) + 11
+
+
+def test_cx_alignment_zag_zig():
+    input_vector = Statevector.from_label('++++++++++++')
+
+    qc = QuantumCircuit(12)
+    qc.cx(2, 1)
+    qc.cx(5, 4)
+    qc.cx(8, 7)
+    qc.cx(11, 10)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 12 * (4 * 2) + 12
+
+
+def test_cx_shifting_rotations():
+    qc = QuantumCircuit(3)
+    qc.cx(2, 1)
+    qc.h(0)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    visualiser.plot_graph(bw_nx_graph)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 3 * (4 * 2) + 3
+
+def test_cx_shifting_rotations_duplication():
+    qc = QuantumCircuit(5)
+    qc.cx(0, 1)
+    qc.h(2)
+    qc.cx(1, 2)
+    qc.cx(4, 3)
+
+    bw_nx_graph = gen_test_graph(qc)
+
+    visualiser.plot_graph(bw_nx_graph)
+
+    # input qubits * (brick depth * number of bricks) + output qubits
+    assert bw_nx_graph.number_of_nodes() == 5 * (4 * 2) + 5
+
+
+
+
+