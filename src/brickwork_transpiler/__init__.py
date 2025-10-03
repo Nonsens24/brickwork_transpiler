a6234f147b1685d7d902ac5e08eefa2c2ceb84d8
Rex <r.fleur@student.tudelft.nl>
2025-10-03 18:24:36 +0200

    Cleanup and continuous integration support

---
+++ src/brickwork_transpiler/__init__.py
@@ -0,0 +1,6 @@
+# src/brickwork_transpiler/__init__.py
+__all__ = [
+    "bfk_encoder", "bricks", "brickwork_transpiler", "circuits",
+    "decomposer", "graph_builder", "gx_angle_extractor", "noise",
+    "pattern_converter", "utils", "visualiser", "algorithms",
+]
--- tests/system_tests/test_circuits.py
+++ tests/system_tests/test_circuits.py
@@ -2,6 +2,8 @@ import numpy as np
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
 from src.brickwork_transpiler import brickwork_transpiler, visualiser, utils, circuits
+from brickwork_transpiler import utils
+
 from src.brickwork_transpiler.utils import calculate_ref_state_from_qiskit_circuit, undo_layout_on_state, \
     extract_logical_to_physical, reference_state_auto
 
